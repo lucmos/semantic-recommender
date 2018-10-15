f9d5bc1cd74709a67841d42b829eb3a009664f23
FedericaSpini <spini.1588482@studenti.uniroma1.it>
2018-10-15 11:53:12 +0200

    -

--- src/main/java/executors/ReadingPhaseExecutor.java
+++ src/main/java/executors/ReadingPhaseExecutor.java
@@ -12,15 +12,20 @@ public class ReadingPhaseExecutor
     public static void main(String[] args){
         Dataset wikimid = new Dataset("wikimid");
         System.out.println(wikimid.toString());
+
         DatasetReader dReader = new DatasetReader();
         System.out.println(dReader.toString());
+
         TsvFileReader tsvReader = new TsvFileReader();
         System.out.println(tsvReader.toString());
         System.out.println(DatasetConstants.WIKIMID_FRIEND_BASED_DATASET.getPath());
-        ArrayList<String> lines = tsvReader.readText(DatasetConstants.WIKIMID_FRIEND_BASED_DATASET.getPath(), 4000000);
+
+        ArrayList<String> lines = tsvReader.readText(DatasetConstants.WIKIMID_FRIEND_BASED_DATASET.getPath(), 40000000);
 //        ArrayList<String> lines = tsvReader.readText("prova_friend_based_dataset.txt", 10);
         System.out.println(lines.size());
         System.out.println(lines.get(0));
+
+
 //        ArrayList<ArrayList<String>> splittedLines = tsvReader.splitByChar(lines);
 
 
