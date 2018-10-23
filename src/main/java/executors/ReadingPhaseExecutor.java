fa7303fb8a3416494d3a87c8d505800b402184f9
Luca Moschella <luca.moschella94@gmail.com>
2018-10-23 22:09:22 +0200

    -

--- src/main/java/executors/ReadingPhaseExecutor.java
+++ src/main/java/executors/ReadingPhaseExecutor.java
@@ -1,16 +1,13 @@
 package executors;
 
-import constants.DatasetInfo;
 import constants.DatasetName;
 import constants.Dimension;
 import datasetsreader.Dataset;
-import datasetsreader.DatasetReader;
-import io.CacheManager;
+import io.Cache;
 
-import javax.rmi.CORBA.Util;
-import javax.xml.crypto.Data;
-
+@Deprecated
 public class ReadingPhaseExecutor {
+
     public static void main(String[] args) {
 
         long start = System.currentTimeMillis();
@@ -18,13 +15,13 @@ public class ReadingPhaseExecutor {
             System.out.println("Reading... " + name);
 
 //            Dataset d = DatasetReader.readDataset(name, Dimension.COMPLETE);
-//            CacheManager.writeToCache(name, d);
+//            Cache.writeToCache(name, d);
 
-          Dataset d = CacheManager.readFromCache(name, Dimension.COMPLETE);
+            Dataset d = Cache.readFromCache(name, Dimension.COMPLETE);
 
             System.out.println(d);
             long now = System.currentTimeMillis();
-            System.out.println("In: " + (now - start)/ 1000.0);
+            System.out.println("In: " + (now - start) / 1000.0);
             start = now;
 
             System.out.println();
--- src/main/java/executors/Test.java
+++ src/main/java/executors/Test.java
@@ -1,4 +1,14 @@
 package executors;
 
+import constants.DatasetName;
+import constants.Dimension;
+import datasetsreader.Dataset;
+import io.Cache;
+
 public class Test {
+    public static void main(String[] args) {
+        Dataset d = Cache.readFromCache(DatasetName.S22, Dimension.SMALL);
+
+        d.getPages().get(0);
+    }
 }
