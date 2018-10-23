7bc854e8af665f9ed22a09189ea9e32342108602
Luca Moschella <luca.moschella94@gmail.com>
2018-10-23 14:24:51 +0200

    Cleaning

--- src/main/java/executors/ReadingPhaseExecutor.java
+++ src/main/java/executors/ReadingPhaseExecutor.java
@@ -30,35 +30,4 @@ public class ReadingPhaseExecutor {
             System.out.println();
         }
     }
-
-
-
-
-//        CacheManager.writeToCache(name);
-//
-//        Dataset d = CacheManager.readFromCache(name);
-//        System.out.println(d.getUsers().size());
-
-//        Dataset d = DatasetReader.readDataset(DatasetName.S23);
-//        Dataset wikimid = new Dataset("wikimid");
-//        System.out.println(wikimid.toString());
-//
-//        DatasetReader dReader = new DatasetReader();
-//        System.out.println(dReader.toString());
-//
-//        TsvFileReader tsvReader = new TsvFileReader();
-//        System.out.println(tsvReader.toString());
-//        System.out.println(DatasetInfo.WIKIMID_FRIEND_BASED_DATASET.getPath());
-//
-//        ArrayList<String> lines = tsvReader.readText(DatasetInfo.WIKIMID_FRIEND_BASED_DATASET.getPath(), 400000000);
-        //        ArrayList<String> lines = tsvReader.readText("prova_friend_based_dataset.txt", 10);
-//        System.out.println(lines.size());
-//        System.out.println(lines.get(0));
-
-
-//        ArrayList<ArrayList<String>> splittedLines = tsvReader.splitByChar(lines);
-
-
-//        dReader.addDataInDataset(wikimid, readedData, DatasetReader.DatasetType.WIKI_MID_FRIENDBASED_DATASET);
-
 }
