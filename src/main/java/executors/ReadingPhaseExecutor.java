53e1c4170df85cca48cc3f6784d8ff9dc54ffaef
Luca Moschella <luca.moschella94@gmail.com>
2018-10-29 22:12:42 +0100

    -

--- src/main/java/executors/ReadingPhaseExecutor.java
+++ src/main/java/executors/ReadingPhaseExecutor.java
@@ -1,5 +1,6 @@
 package executors;
 
+import babelnet.WikiPageMapping;
 import constants.DatasetName;
 import constants.Dimension;
 import datasetsreader.Dataset;
@@ -12,15 +13,17 @@ public class ReadingPhaseExecutor {
 
     public static void main(String[] args) throws Utils.CacheNotPresent {
 
-        for (DatasetName name : DatasetName.values()) {
-           Chrono c = new Chrono("Reading... " + name);
-
-//            Dataset d = DatasetReader.readDataset(name, Dimension.COMPLETE);
-//            Cache.writeToCache(name, d);
-
-            Dataset d = Cache.DatasetCache.readFromCache(name, Dimension.SMALL);
-            System.out.println(d);
-            c.millis();
-        }
+//        for (DatasetName name : DatasetName.values()) {
+//           Chrono c = new Chrono("Reading... " + name);
+//
+////            Dataset d = DatasetReader.readDataset(name, Dimension.COMPLETE);
+////            Cache.writeToCache(name, d);
+//
+////            Dataset d = Cache.DatasetCache.readFromCache(name, Dimension.SMALL);
+//            c.millis();
+//        }
+        WikiPageMapping d = Cache.WikiMappingCache.readFromCache(Dimension.SMALL);
+        System.out.println(d);
+        System.out.println(d.stats());
     }
 }
