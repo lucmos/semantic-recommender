a2f96a2b3b4bd216d94cdfea44938b0c41b60754
Luca Moschella <luca.moschella94@gmail.com>
2018-10-30 19:44:48 +0100

    Extend the functionalities of the Counter class

--- src/main/java/utils/Counter.java
+++ src/main/java/utils/Counter.java
@@ -5,22 +5,78 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Counter<T> {
-    private final Map<T, Integer> counts = new HashMap<>();
+    private final Map<T, Double> counts = new HashMap<>();
+
+    public Counter() {
+    }
+
+    public static <E, N, V extends Collection<N>> Counter<N> fromMultiMap(Map<E, V> map) {
+        Counter<N> c = new Counter<>();
+        for (V value : map.values()) {
+            c.increment(value);
+        }
+        return c;
+    }
+
+    public static <E, N> Counter<N> fromMap(Map<E, N> map) {
+        Counter<N> c = new Counter<>();
+        for (N value : map.values()) {
+            c.increment(value);
+        }
+        return c;
+    }
 
     public Counter<T> increment(T t) {
-        counts.merge(t, 1, Integer::sum);
+        counts.merge(t, 1d, Double::sum);
         return this;
     }
 
-    public Counter<T> increment(Iterable<T> iterable) {
-        for (T e : iterable) {
+    public Counter<T> increment(Collection<T> coll) {
+        for (T e : coll) {
             this.increment(e);
         }
         return this;
     }
 
+    public String getDistribution(int k) {
+        List<T> list = k < 0 ? mostCommon(counts.size()) : mostCommon(k);
+
+        StringBuilder s = new StringBuilder();
+        for (T e : list) {
+            s.append(String.format("\t%s\t\t%s\n", e, count(e)));
+        }
+        return s.toString();
+    }
+
+    public String getDistribution() {
+        return getDistribution(-1);
+    }
+
     public int count(T t) {
-        return counts.getOrDefault(t, 0);
+        return (int) get(t);
+    }
+    public double importance(T t) {
+        return get(t);
+    }
+
+    public int total() {
+        return counts.values().stream().mapToInt(Double::intValue).sum();
+    }
+
+    public double totalImportance() {
+        return counts.values().stream().mapToDouble(Double::doubleValue).sum();
+    }
+
+    public double get(T t) {
+        return counts.getOrDefault(t, 0d);
+    }
+
+    public void set(T t, double v) {
+        counts.put(t, v);
+    }
+
+    public int size() {
+        return counts.size();
     }
 
     public List<T> mostCommon(int k) {
@@ -40,17 +96,7 @@ public class Counter<T> {
         return counts.toString();
     }
 
-    public String getDistribution(int k) {
-        List<T> list = k < 0 ? mostCommon(counts.size()) : mostCommon(k);
-
-        StringBuilder s = new StringBuilder();
-        for (T e : list) {
-            s.append(String.format("\t%s\t\t%s\n", e, count(e)));
-        }
-        return s.toString();
-    }
-
-    public String getDistribution() {
-        return getDistribution(-1);
+    public Map<T, Double> getMap() {
+        return counts;
     }
 }
