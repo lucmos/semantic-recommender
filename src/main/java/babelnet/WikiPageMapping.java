591843d67f4e2199d2ff446119d9c0595aafbae4
Luca Moschella <luca.moschella94@gmail.com>
2018-11-01 17:27:35 +0100

    Refactor of the babelnet interface

--- src/main/java/babelnet/WikiPageMapping.java
+++ src/main/java/babelnet/WikiPageMapping.java
@@ -1,9 +1,5 @@
 package babelnet;
 
-import constants.DatasetName;
-import twittermodel.Dataset;
-import io.Cache;
-import io.Utils;
 import it.uniroma1.lcl.babelnet.BabelNet;
 import it.uniroma1.lcl.babelnet.BabelSynset;
 import it.uniroma1.lcl.babelnet.data.BabelCategory;
@@ -11,124 +7,87 @@ import it.uniroma1.lcl.babelnet.resources.WikipediaID;
 import it.uniroma1.lcl.jlt.util.Language;
 import it.uniroma1.lcl.kb.Domain;
 import twittermodel.WikiPageModel;
-import utils.Chrono;
-import utils.Counter;
 import utils.IndexedSerializable;
-import utils.Statistics;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class WikiPageMapping implements IndexedSerializable {
     private WikiPageMapping() {
     }
-
-//    private static Map<Integer, BabelSynset> wikiToSynsetObjCache = new HashMap<>();
-//    private static Map<BabelSynset, Set<BabelCategory>> synsetToCategoriesObjCache = new HashMap<>();
-//    private static Map<BabelSynset, Set<Domain>> synsetToDomainObjCache = new HashMap<>();
-
-    private static BabelSynset getSynsetObj(WikiPageModel wikiPage) {
-        assert (wikiPage != null);
-
-//        if (wikiToSynsetObjCache.containsKey(wikiPage.getId())) {
-//            return wikiToSynsetObjCache.get(wikiPage.getId());
-//        }
-//
-        BabelSynset synset = BabelNet.getInstance().getSynset(new WikipediaID(wikiPage.getSimpleName(), Language.EN));
-//        wikiToSynsetObjCache.put(wikiPage.getId(), synset);
-//
-        return synset;
-    }
-
-    private static Set<BabelCategory> getCategoriesObj(BabelSynset synset) {
-        assert synset != null;
-
-//        if (synsetToCategoriesObjCache.containsKey(synset)) {
-//            return synsetToCategoriesObjCache.get(synset);
-//        }
-
-        Set<BabelCategory> l = synset.getCategories().stream().filter(x -> x.getLanguage().equals(Language.EN)).collect(Collectors.toSet());
-//        synsetToCategoriesObjCache.put(synset, l);
-        return l;
-    }
-
-    private static Set<Domain> getDomainsObj(BabelSynset synset) {
-        assert synset != null;
-//
-//        if (synsetToDomainObjCache.containsKey(synset)) {
-//            return synsetToDomainObjCache.get(synset);
-//        }
-
-        Set<Domain> l = synset.getDomains().keySet();
-//        synsetToDomainObjCache.put(synset, l);
-        return l;
-    }
-
-//    private static Map<Integer, String> wikiToSynsetCache = new HashMap<>();
-//    private static Map<Integer, Set<String>> wikiToCategoriesCache = new HashMap<>();
-//    private static Map<Integer, Set<String>> wikiToDomainCache = new HashMap<>();
-//
     public static String getSynset(WikiPageModel pageModel) {
         assert pageModel != null;
 
-//        if (wikiToSynsetCache.containsKey(pageModel.getId())) {
-//            return wikiToSynsetCache.get(pageModel.getId());
-//        }
-
         BabelSynset synId = getSynsetObj(pageModel);
-        String out = synId == null ? null : synId.getID().getID();
 
-//        wikiToSynsetCache.put(pageModel.getId(), out);
-
-        return out;
+        return synId == null ? null : synId.getID().getID();
     }
 
     public static Set<String> getCategories(WikiPageModel pageModel) {
         assert pageModel != null;
-
-//        if (wikiToCategoriesCache.containsKey(pageModel.getId())) {
-//            return wikiToCategoriesCache.get(pageModel.getId());
-//        }
-
-        BabelSynset synId = getSynsetObj(pageModel);
-
-        HashSet<String> catSet = new HashSet<>();
-        if (synId != null) {
-            Set<BabelCategory> cats = getCategoriesObj(synId);
-
-            for (BabelCategory b : cats) {
-                catSet.add(b.toString());
-            }
-
-//            wikiToCategoriesCache.put(pageModel.getId(), catSet);
-//            return catSet;
-        }
-
-//        wikiToCategoriesCache.put(pageModel.getId(), catSet);
-        return catSet;
+        return getCategories(getSynsetObj(pageModel));
     }
 
     public static Set<String> getDomains(WikiPageModel pageModel) {
         assert pageModel != null;
+        return getDomains(getSynsetObj(pageModel));
+    }
 
-//        if (wikiToDomainCache.containsKey(pageModel.getId())) {
-//            return wikiToDomainCache.get(pageModel.getId());
-//        }
+//  caching to improve performances.
 
-        BabelSynset synId = getSynsetObj(pageModel);
+    private static Map<Integer, BabelSynset> wikiToSynsetObjCache = new HashMap<>();
+    private static Map<BabelSynset, Set<String>> synsetToCategoriesObjCache = new HashMap<>();
+    private static Map<BabelSynset, Set<String>> synsetToDomainObjCache = new HashMap<>();
+
+    private static BabelSynset getSynsetObj(WikiPageModel wikiPage) {
+        assert (wikiPage != null);
+
+        if (wikiToSynsetObjCache.containsKey(wikiPage.getId())) {
+            return wikiToSynsetObjCache.get(wikiPage.getId());
+        }
+
+        BabelSynset synset = BabelNet.getInstance().getSynset(new WikipediaID(wikiPage.getSimpleName(), Language.EN));
+        wikiToSynsetObjCache.put(wikiPage.getId(), synset);
+
+        return synset;
+    }
+
+    private static Set<String> getCategories(BabelSynset synset) {
+        if (synsetToCategoriesObjCache.containsKey(synset)) {
+            return synsetToCategoriesObjCache.get(synset);
+        }
+
+        HashSet<String> catSet = new HashSet<>();
+
+        if (synset != null) {
+            Set<BabelCategory> cats = synset.getCategories().stream().filter(x -> x.getLanguage().equals(Language.EN)).collect(Collectors.toSet());
+            for (BabelCategory b : cats) {
+                catSet.add(b.toString());
+            }
+        }
+
+        synsetToCategoriesObjCache.put(synset, catSet);
+        return catSet;
+    }
+
+    private static Set<String> getDomains(BabelSynset synset) {
+        if (synsetToDomainObjCache.containsKey(synset)) {
+            return synsetToDomainObjCache.get(synset);
+        }
 
         HashSet<String> domSet = new HashSet<>();
-        if (synId != null) {
-            Set<Domain> doms = getDomainsObj(synId);
 
+        if (synset != null) {
+            Set<Domain> doms = synset.getDomains().keySet();
             for (Domain b : doms) {
                 domSet.add(b.toString());
             }
-
-//            wikiToDomainCache.put(pageModel.getId(), domSet);
-//            return domSet;
         }
-//        wikiToDomainCache.put(pageModel.getId(), domSet);
+
+        synsetToDomainObjCache.put(synset, domSet);
         return domSet;
     }
 }
