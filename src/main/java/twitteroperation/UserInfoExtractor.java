59a54b745cdd1240e8957483d173926db52cd215
FedericaSpini <spini.1588482@studenti.uniroma1.it>
2018-11-07 10:55:37 +0100

    -

--- src/main/java/twitteroperation/UserInfoExtractor.java
+++ src/main/java/twitteroperation/UserInfoExtractor.java
@@ -14,38 +14,29 @@ import java.util.Map;
 
 public class UserInfoExtractor
 {
-
-//    public void getUserFollowing(int userId) throws TwitterException {
-//
-//        Twitter twitter = new TwitterFactory().getInstance();
-////        long cursor = -1;
-//        IDs ids = twitter.getOutgoingFriendships(userId);
-//        for (long i : ids.getIDs()){
-//            System.out.println(userId +": "+ i );
-//        }
-//
-//    }
-
     /**
      * This method look for all the following of the users of the dataset. If these users
      * already exist a following relation is added.
      * @param dataset the method look for following of the users in this dataset
      * @throws TwitterException
      */
-    public void getUsersFollowing(Dataset dataset) throws TwitterException {
+    public void getUsersFollowing(Dataset dataset, Dataset clusterizedDataset, TwitterFactory twitterFactory) throws TwitterException {
         try{
             Map<Integer, UserModel> users = dataset.getUsers();
             Twitter twitter = new TwitterFactory().getInstance();
             for (Map.Entry<Integer, UserModel> entry: users.entrySet()) {
+                System.out.println(entry.getKey());
                 long userId = Integer.parseInt(entry.getValue().getName(dataset.getIdMapping()));
-                System.out.println(userId);
                 IDs ids = twitter.getFriendsIDs(userId,-1);
-                System.out.println(ids + " "+ids.getIDs());
-    //            for (long i : ids.getIDs()) {
-    //                System.out.println(userId + ": " + i);
-    //                //fare "se i sta nella lista di quelli conosciuti, allora aggiungilo come following per lo user corrispondente")
+                System.out.println(userId+" follows "+ ids.getIDs().length+ " users");
+                for (long i : ids.getIDs()) {
+                    if (clusterizedDataset.exixstObj(i+"")) {
+                        UserModel u = twitterFactory.getInstance(i+"");
+                        dataset.addUser(u);
+                    }
                 }
-        }
+
+            }}
         catch (TwitterException e){
             e.printStackTrace();
             System.out.println("Failed to get outgoing friendships: "+ e.getMessage());
@@ -56,8 +47,9 @@ public class UserInfoExtractor
     public static void main(String[] args) throws Utils.CacheNotPresent, TwitterException {
         UserInfoExtractor userInfoExtractor = new UserInfoExtractor();
 
-        Dataset d = Cache.DatasetCache.read(DatasetName.S22);
-        Map<Integer, UserModel> users = d.getUsers();
+        Dataset d22 = Cache.DatasetCache.read(DatasetName.S22);
+        Dataset wikimid = Cache.DatasetCache.read(DatasetName.WIKIMID);
+        Map<Integer, UserModel> users = d22.getUsers();
 
         ConfigurationBuilder cfg = new ConfigurationBuilder();
 
@@ -68,37 +60,8 @@ public class UserInfoExtractor
 
         cfg.setTweetModeExtended(true);
 
-        userInfoExtractor.getUsersFollowing(d);
-
-//        for (Map.Entry<Integer, UserModel> entry: users.entrySet()){
-////            userInfoExtractor.getUserFollowing(Integer.parseInt(entry.getValue().getIdString()));
-//        }
+        userInfoExtractor.getUsersFollowing(d22);
 
 
-
-//        StatusListener listener = new StatusListener()
-//        {
-//            public void onStatus(Status status) {
-//                System.out.println(status.getUser().getName() + " : "
-//                        + status.getText());
-//            }
-//            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
-//            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {}
-//
-//            public void onScrubGeo(long l, long l1) {}
-//
-//            public void onStallWarning(StallWarning stallWarning) {}
-//
-//            public void onException(Exception ex) { ex.printStackTrace(); }
-//        };
-//
-//
-//        TwitterStream twitterStream = new TwitterStreamFactory(cfg.build()).getInstance();
-//        twitterStream.addListener(listener);
-//        FilterQuery fq = new FilterQuery();
-//        fq.language("ita");
-//        twitterStream.filter(fq);
-//        twitterStream.sample();
-//
     }
 }
