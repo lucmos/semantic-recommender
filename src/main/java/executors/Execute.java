e8b8a2563ed0cc75aec25e55bba33019867e8b9a
Luca Moschella <luca.moschella94@gmail.com>
2018-11-17 14:05:35 +0100

    add comment on requirements

--- src/main/java/executors/Execute.java
+++ src/main/java/executors/Execute.java
@@ -6,6 +6,7 @@ import io.Utils;
 
 public class Execute {
     public static void main(String[] args) throws Utils.CacheNotPresent {
+//      Must be present the twitter downloaded information! See TwitterRespPath
         Cache.DatasetCache.regenCache();
         DatasetMerger.unionAndSave();
         Cache.JavaExportCache.export();
