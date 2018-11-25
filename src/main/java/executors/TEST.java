0ebad8deed936c628cdcaca8b7beeee2a6bc8f83
FedericaSpini <spini.1588482@studenti.uniroma1.it>
2018-11-25 18:38:39 +0100

    Auto stash before merge of "dev" and "origin/dev"

--- src/main/java/executors/TEST.java
+++ src/main/java/executors/TEST.java
@@ -9,7 +9,7 @@ import model.twitter.WikiPageModel;
 
 public class TEST {
     public static void main(String[] args) throws Utils.CacheNotPresent {
-        Dataset d = Cache.DatasetCache.read(DatasetName.WIKIMID);
+        Dataset d = Cache.DatasetCache.read(DatasetName.S21);
 
         System.out.println(d.report());
 
